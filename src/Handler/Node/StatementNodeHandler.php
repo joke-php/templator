4cca41fe219f0ff3b0964663e4d2438fa800fc75
Alexander Vorobyev <voral.adm@gmail.com>
2026-06-08 07:21:51 +0300

    feat: StatementNodeHandler

--- src/Handler/Node/StatementNodeHandler.php
+++ src/Handler/Node/StatementNodeHandler.php
@@ -4,15 +4,50 @@ declare(strict_types=1);
 
 namespace Vasoft\Joke\Templator\Handler\Node;
 
+use Vasoft\Joke\Container\ServiceContainer;
+use Vasoft\Joke\Exceptions\JokeException;
 use Vasoft\Joke\Templator\Contracts\NodeProcessorInterface;
 use Vasoft\Joke\Templator\Contracts\Handler\NodeHandlerInterface;
 use Vasoft\Joke\Templator\Contracts\Parser\NodeInterface;
-use Vasoft\Joke\Templator\Parser\Node\BlockNode;
+use Vasoft\Joke\Templator\Exceptions\CompileException;
+use Vasoft\Joke\Templator\Exceptions\RenderingException;
+use Vasoft\Joke\Templator\Parser\Node\StatementNode;
+use Vasoft\Joke\Templator\TemplatorConfig;
 
+/**
+ * Обработчик узлов блоков с непарными директивами.
+ *
+ * Выступает в роли диспетчера: определяет тип директивы (например, 'csrf')
+ * и делегирует обработку соответствующему специализированному хендлеру.
+ * Использует контейнер зависимостей для ленивой загрузки и кеширования экземпляров хендлеров.
+ */
 class StatementNodeHandler implements NodeHandlerInterface
 {
+    /** @var array<string,NodeHandlerInterface> */
+    private array $instantiatedHandler = [];
     /**
-     * @inherit
+     * @var class-string Ожидаемый класс ноды
+     */
+    protected string $nodeClass = StatementNode::class;
+
+    /**
+     * Создает новый обработчик блоков.
+     *
+     * @param ServiceContainer $container контейнер зависимостей для получения хендлеров директив
+     * @param TemplatorConfig  $config    конфигурация шаблонизатора, содержащая маппинг директив
+     */
+    public function __construct(
+        protected readonly ServiceContainer $container,
+        protected readonly TemplatorConfig $config,
+    ) {}
+
+    /**
+     * {@inheritDoc}
+     *
+     * Делегирует компиляцию узла специализированному хендлеру директивы.
+     *
+     * @throws CompileException если передан узел неверного типа или хендлер директивы не найден
+     * @throws JokeException    если хендлер не зарегистрирован в конфигурации
      */
     public function compile(
         NodeInterface $node,
@@ -20,22 +55,74 @@ class StatementNodeHandler implements NodeHandlerInterface
         array $context,
         array $localVars = [],
     ): string {
-        assert($node instanceof BlockNode);
+        if (!$node instanceof $this->nodeClass) {
+            throw new CompileException($this->getErrorMessage($node));
+        }
 
-        return 'Compile';
+
+        $handler = $this->getDirectiveHandler($node->directive);
+
+        return $handler->compile($node, $processor, $context, $localVars);
     }
 
     /**
-     * @inherit
+     * Получает экземпляр хендлера для указанной директивы.
+     *
+     * Использует внутренний кэш для повышения производительности. Если хендлер еще не создан,
+     * регистрирует его в контейнере как синглтон и извлекает оттуда.
+     *
+     * @param string $directive имя директивы (например, 'if', 'foreach')
+     *
+     * @return NodeHandlerInterface экземпляр обработчика директивы
+     *
+     * @throws JokeException если хендлер не зарегистрирован в конфигурации
+     */
+    private function getDirectiveHandler(string $directive): NodeHandlerInterface
+    {
+        if (!isset($this->instantiatedHandler[$directive])) {
+            $index = $directive . '#DirectiveHandler';
+            if (!$this->container->has($index)) {
+                $this->container->registerSingleton($index, $this->config->getDirectiveHandler($directive));
+            }
+            /** @var NodeHandlerInterface $handler */
+            $handler = $this->container->get($index);
+            $this->instantiatedHandler[$directive] = $handler;
+        }
+
+        return $this->instantiatedHandler[$directive];
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * Делегирует рендеринг узла специализированному хендлеру директивы.
+     *
+     * @throws RenderingException если передан узел неверного типа или хендлер директивы не найден
+     * @throws JokeException      если хендлер не зарегистрирован в конфигурации
      */
     public function render(
         NodeInterface $node,
         NodeProcessorInterface $processor,
         array $context,
-        array $localVars = [],
     ): string {
-        assert($node instanceof BlockNode);
+        if (!$node instanceof $this->nodeClass) {
+            throw new RenderingException($this->getErrorMessage($node));
+        }
 
-        return 'Render';
+        $handler = $this->getDirectiveHandler($node->directive);
+
+        return $handler->render($node, $processor, $context);
+    }
+
+    /**
+     * Формирует сообщение об ошибке несоответствия типа узла.
+     *
+     * @param NodeInterface $node фактически полученный узел
+     *
+     * @return string текст ошибки
+     */
+    protected function getErrorMessage(NodeInterface $node): string
+    {
+        return sprintf('Expected instance of %s, got %s.', $this->nodeClass, $node::class);
     }
 }
